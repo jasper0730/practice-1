a14f608937f53e88471a8ecdd310f6225c0960bb
jasper0730 <wilsonlee0730@gmail.com>
2022-07-10 23:22:36 +0800

    動畫

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,58 @@
 // size的active
-$('.size > div').on('click', function () {
+$(".size > div").on("click", function () {
   const $this = $(this);
 
-  if ($this.hasClass('active')) {
-    $this.removeClass('active')
+  if ($this.hasClass("active")) {
+    $this.removeClass("active");
+  } else {
+    $(".size > div").removeClass("active");
+    $this.addClass("active");
   }
-  else {
-    $(".size > div").removeClass('active')
-    $this.addClass('active')
-  }
-})
+});
 
 // color的active
-$('.color > div > img').on('click', function () {
+$(".color > div > img").on("click", function () {
   const $this = $(this);
+  if ($this.hasClass("active")) {
+    $this.removeClass("active");
+  } else {
+    $(".color > div > img").removeClass("active");
+    $this.addClass("active");
+  }
+});
 
-  if ($this.hasClass('active')) {
-    $this.removeClass('active')
+// accordion
+$(".accordion").on("click", function () {
+  // const $title = $(this).find(".title");
+  const $collapse = $(this).find(".collapse");
+  if ($(this).hasClass("active")) {
+    // 關閉
+    $(this).removeClass("active");
+    $collapse.slideUp();
+  } else {
+    // 打開
+    $(this).addClass("active");
+    $collapse.slideDown();
   }
-  else {
-    $(".color > div > img").removeClass('active')
-    $this.addClass('active')
-  }
-})
+});
+
+// 點選menu跳到指定區塊
+$(".article > .menu > p").on("click", function (e) {
+  const targetBlock = e.target.dataset.name;
+  $("html, body").animate(
+    {
+      scrollTop: $(`#${targetBlock}`).offset().top,
+    },
+    { duration: 500, easing: "swing" }
+  );
+  return false;
+});
+
+// 回到最上方
+$(".go-top").on("click", function () {
+  $("html,body").animate({ scrollTop: 0 }, "slow");
+  return false;
+});
 
 // swiper
 const swiper = new Swiper(".mySwiper", {
@@ -39,11 +69,11 @@ const swiper = new Swiper(".mySwiper", {
     // when window width is >= 640px
     1025: {
       slidesPerView: 3,
-      spaceBetween: 35
+      spaceBetween: 35,
     },
     1441: {
       slidesPerView: 4,
-      spaceBetween: 35
+      spaceBetween: 35,
     },
-  }
+  },
 });
